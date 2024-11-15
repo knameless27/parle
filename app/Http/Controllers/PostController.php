7f3fff11ca1a46a9b1fb0e7c9b7d8e340be0957a
knameless27 <knameless27@gmail.com>
2024-11-14 21:47:26 -0500

    feat: :fire: post controller finished, still making validations

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'user_id' => Auth::id(),
+        ]);
+
+        return response()->json([
+            'message' => 'Post creado exitosamente',
+            'post' => $post,
+        ], 201);
+    }
+
+    public function index($category)
+    {
+        $posts = Post::where('category_id', $category)->get();
+
+        if ($posts->isEmpty()) {
+            return response()->json([
+                'message' => 'No se encontraron posts para esta categoría',
+            ], 404);
+        }
+
+        return response()->json([
+            'posts' => $posts,
+        ], 200);
+    }
 }
