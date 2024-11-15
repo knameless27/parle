cc2e85a540d1406cd2c363c45eb9c7e324f2d3cf
knameless27 <knameless27@gmail.com>
2024-11-14 22:01:07 -0500

    feat: :fire: adding sanctum to the project

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::get('/posts/{category}', [PostController::class, 'index']);
+});
